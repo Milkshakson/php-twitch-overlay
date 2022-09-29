f0738f2f40a4c0e7554e826f73af83c935613530
Jeferson Josias Costa <jeferson@sejuf.pr.gov.br>
2022-09-29 13:41:40 -0300

    Constatnt APPPAPH corrigida

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 defined('ROOTDIR') or define('ROOTDIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
-defined('APPPATH') or define('APPPATH', ROOTDIR . DIRECTORY_SEPARATOR . 'App' . DIRECTORY_SEPARATOR);
+defined('APPPATH') or define('APPPATH', ROOTDIR . 'App' . DIRECTORY_SEPARATOR);
 include_once 'displayErrors.php';
 require_once('App/Core/App.php');
 require_once ROOTDIR . 'vendor/autoload.php';
