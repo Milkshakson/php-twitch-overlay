7e467fd44f3a15b13ee95e9fa1c6e7e31bb60646
Jeferson Josias Costa <jeferson@sejuf.pr.gov.br>
2022-09-29 14:50:29 -0300

    DisplayErroros somente em dev

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 defined('ROOTDIR') or define('ROOTDIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 defined('APPPATH') or define('APPPATH', ROOTDIR . 'App' . DIRECTORY_SEPARATOR);
-include_once 'displayErrors.php';
+if (ENVIRONMENT != 'production')
+    include_once 'displayErrors.php';
 require_once('App/Core/App.php');
 require_once ROOTDIR . 'vendor/autoload.php';
 require_once ROOTDIR . 'App/Routes/Routes.php';
