2626b57dbd77a30fbf5c82bd3da39ca5235e285b
Jeferson Costa <jeferson_cta@hotmail.com>
2022-10-01 17:42:45 -0300

    Ajustes

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 defined('ROOTDIR') or define('ROOTDIR', dirname(__FILE__) . DIRECTORY_SEPARATOR);
 defined('APPPATH') or define('APPPATH', ROOTDIR . 'App' . DIRECTORY_SEPARATOR);
-if (ENVIRONMENT != 'production')
-    include_once 'displayErrors.php';
 require_once('App/Core/App.php');
 require_once ROOTDIR . 'vendor/autoload.php';
 require_once ROOTDIR . 'App/Routes/Routes.php';
